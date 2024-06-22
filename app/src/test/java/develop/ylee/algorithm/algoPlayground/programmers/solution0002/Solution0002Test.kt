43b77fbb6bb486bd6cef4c4d78e7d35b6a8f05d0
ylee <ylee.develop@gmail.com>
2024-06-23 08:59:22 +0900

    set solution0002 question & test

--- app/src/test/java/develop/ylee/algorithm/algoPlayground/programmers/solution0002/Solution0002Test.kt
+++ app/src/test/java/develop/ylee/algorithm/algoPlayground/programmers/solution0002/Solution0002Test.kt
@@ -1,4 +1,45 @@
 package develop.ylee.algorithm.algoplayground.programmers.solution0002
 
+import org.junit.Assert.assertArrayEquals
+import org.junit.Test
+
 class Solution0002Test {
+
+    private val solution = Solution0002()
+
+    @Test
+    fun `returns correct result for multiple rounds`() {
+        val n = 5
+        val info = intArrayOf(2,1,1,1,0,0,0,0,0,0,0)
+        val expected = intArrayOf(0,2,2,0,1,0,0,0,0,0,0)
+        val result = solution.solution(n, info)
+        assertArrayEquals(expected, result)
+    }
+
+    @Test
+    fun `returns -1 for single round`() {
+        val n = 1
+        val info = intArrayOf(1,0,0,0,0,0,0,0,0,0,0)
+        val expected = intArrayOf(-1)
+        val result = solution.solution(n, info)
+        assertArrayEquals(expected, result)
+    }
+
+    @Test
+    fun `returns correct result for multiple rounds with same scores`() {
+        val n = 9
+        val info = intArrayOf(0,0,1,2,0,1,1,1,1,1,1)
+        val expected = intArrayOf(1,1,2,0,1,2,2,0,0,0,0)
+        val result = solution.solution(n, info)
+        assertArrayEquals(expected, result)
+    }
+
+    @Test
+    fun `returns correct result for multiple rounds with different scores`() {
+        val n = 10
+        val info = intArrayOf(0,0,0,0,0,0,0,0,3,4,3)
+        val expected = intArrayOf(1,1,1,1,1,1,1,1,0,0,2)
+        val result = solution.solution(n, info)
+        assertArrayEquals(expected, result)
+    }
 }
