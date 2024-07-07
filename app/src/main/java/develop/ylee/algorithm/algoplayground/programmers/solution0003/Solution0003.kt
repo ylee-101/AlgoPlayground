c34f72f119ebc68ce1e8275ca9ecea027401c101
ylee <ylee.develop@gmail.com>
2024-07-07 19:10:15 +0900

    정확도높고 시간초과나는 케이스

--- app/src/main/java/develop/ylee/algorithm/algoplayground/programmers/solution0003/Solution0003.kt
+++ app/src/main/java/develop/ylee/algorithm/algoplayground/programmers/solution0003/Solution0003.kt
@@ -1,42 +1,31 @@
 package develop.ylee.algorithm.algoplayground.programmers.solution0003
 
 class Solution0003 {
-
+    var stones: IntArray = intArrayOf()
     fun solution(stones: IntArray, k: Int): Int {
         val stoneList = stones.toList()
-        var min = stoneList.min()
-        var max = stoneList.max()
-        var answer : Int = (min + max) / 2
-        while (min < max){
-            if (canGo(stones, answer, k)) {
-                min = answer
-                answer = (min + max) / 2
-                if (min == answer) return answer
-            } else {
-                max = answer
-                answer = (min + max) / 2
-                if (max == answer) return answer
-            }
+        val min = stoneList.minOrNull()?:0
+        val max = stoneList.maxOrNull()?:0
+        if (min == max) return stones.size
+        var answer = 0
+        this.stones = stones
+        while (goStones(k)) {
+            answer++
         }
         return answer
     }
 
-    fun canGo(stones: IntArray, num: Int, base: Int): Boolean {
-        val curSize = stones.size
-        val tmp = IntArray(curSize){idx -> stones[idx] - num}
+    fun goStones(k: Int): Boolean {
+        val size = stones.size
         var idx = 0
-        while (idx < curSize) {
-            while (tmp[idx] > 0) {
-                idx++
-                if (idx == curSize) return true
+        while (idx < size) {
+            stones[idx]--
+            var nextStone = 1
+            while (idx + nextStone < size && stones[idx + nextStone] == 0) {
+                nextStone++
             }
-            var cur = idx
-            while (tmp[cur] <= 0) {
-                cur++
-                if (cur - idx > base) return false
-                if (cur == curSize) return true
-            }
-            idx = cur
+            if (nextStone > k) return false
+            idx += nextStone
         }
         return true
     }
